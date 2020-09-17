9adefb314d44a2b6e2db09ec2055f3d4576b2cf4
xpirep <xpirep@kali.com>
2020-09-18 09:14:16 +1000

    moved around stuff

--- api/backend/mongo.py
+++ api/backend/mongo.py
@@ -3,6 +3,25 @@ from pymongo import MongoClient
 
 COLLECTION_NAME = 'backend'
 
+class Repository(object):
+ def __init__(self, adapter=None):
+   self.client = adapter()
+
+ def find_all(self, selector):
+   return self.client.find_all(selector)
+ 
+ def find(self, selector):
+   return self.client.find(selector)
+ 
+ def create(self, kudo):
+   return self.client.create(kudo)
+  
+ def update(self, selector, kudo, upsert=False):
+   return self.client.update(selector, kudo, upsert)
+  
+ def delete(self, selector):
+   return self.client.delete(selector)
+
 class MongoRepository(object):
  def __init__(self):
    mongo_url = os.environ['MONGO_URL']
